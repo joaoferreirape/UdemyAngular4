dfaf18518d862468192dd5514d2197cdb6535e46
João Ferreira <joaoferreirape@compesa.com.br>
2017-12-26 08:05:17 -0300

    Curso de Angular 4

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,9 +1,13 @@
 import {Spacecraft, Containership} from './base-ships'
 import {MilleniumFalcon} from './starfighters'
 
+import * as _ from 'lodash' // Para definição e checagem de tipos, muito importante
+console.log(_.pad("Typescrits Examples", 40, "="))
+
+
+
 let ship = new Spacecraft('hyperdrive')
 ship.jumIntoHyperspace()
-
 
 
 
@@ -13,7 +17,4 @@ falcon.jumIntoHyperspace()
 
 
 let goodForTheJob = ( ship: Containership ) =>  ship.cargoContainers > 2
-
-
-
 console.log(`Is falcon good for the job? ${goodForTheJob(falcon) ? 'yes' : 'no'}`)
